97ef09a3194e805a4b93f78443487bf979621747
pollo <pollo@MSI>
2026-03-23 11:35:24 -0700

    clase conexion

---
+++ RestauranteNoseCual/Base de Datos/Conexion.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Supabase;
+
+namespace RestauranteNoseCual.Base_de_Datos
+{
+    public class Conexion
+    {
+        
+    }
+}
